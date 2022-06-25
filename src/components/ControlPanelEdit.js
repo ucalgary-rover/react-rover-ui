670429f5373b1c83c5c37c8a46eb85983a4577f1
shivamdesai04 <shivam.desai@ucalgary.ca>
2022-06-25 12:06:07 -0700

    lat and long swap

--- src/components/ControlPanelEdit.js
+++ src/components/ControlPanelEdit.js
@@ -1,58 +1,125 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import EventEmitter2 from 'eventemitter2';
 import ROSLIB from 'roslib';
+import { csv } from 'd3';
+import csvFilePath from './waypoints.csv';
+import { MapView } from './MapView';
+
+
+
+
+
 
 var ros = new ROSLIB.Ros({
-    url : 'ws://localhost:9090'
-  });
-  
-  ros.on('connection', function() {
-    console.log('Connected to websocket server');
-  });
-  
-  ros.on('error', function(error) {
-    console.log('Error connecting to websocket server: ', error);
-  });
-  
-  ros.on('close', function() {
-    console.log('Connection to websocket server closed.');
-  });
-  
-  var listener = new ROSLIB.Topic({
-    ros : ros,
-    name : '/rover/Connections',
-    messageType: 'rover/SignalStatus'
-  });
+  url : 'ws://localhost:9090'
+});
 
-  
-//function component called Connections
-//has four states, bit_rate, link_quality, signal, noise  
-export function Connections(props){
-  const [bit_rate, setBit] = useState("N/A");
-  const [link_quality, setLink] = useState("N/A");
-  const [signal, setSignal] = useState("N/A");
-  const [noise, setNoise] = useState("N/A");
+ros.on('connection', function() {
+  console.log('Connected to websocket server');
+});
 
-  // when you a message,message contains the four states that can change
-  // the current state
-  listener.subscribe(function(message) {
-    setBit(message.bit_rate);
-    setLink(message.link_quality);
-    setSignal(message.signal);
-    setNoise(message.noise);
-  });
+ros.on('error', function(error) {
+  console.log('Error connecting to websocket server: ', error);
+});
+
+ros.on('close', function() {
+  console.log('Connection to websocket server closed.');
+});
+
+var listener = new ROSLIB.Topic({
+  ros : ros,
+  name : '/rover/gps_report',
+  messageType: 'rover/GpsCoords'
+});
+
+// message contains the location of the rover currently
+listener.subscribe(function(message) {
+  console.log('Received message on ' + listener.name + ': ' + message.latitude + " " + message.longitude);
+});
+
+
+
+
+//Function component MapControlPanel with state RouteMode, this is the pannel
+//below
+export function MapControlPanel(props){
+  const [routeMode, setRouteMode] = useState("Edit Route")
+
+
+  //if in edit route mode, return visibility == hidden
+  const visibility = routeMode === "Edit Route" ? "hidden": "visible";
+
+  //onSubmit is the event that triggers addCoordinate
+  function addCoordinate(event){
+    event.preventDefault();
+    if(event.target.longitude.value !== "" && event.target.latitude.value !== ""){
+      const newCoordinate = {long: event.target.longitude.value, lat: event.target.latitude.value};
+      props.addMarker(newCoordinate);
+      event.target.longitude.value = "";
+      event.target.latitude.value = "";
+    }
+  }
+
+  function changeRouteMode() {
+    let newRouteMode = routeMode === "Edit Route" ? "Finish Editing" : "Edit Route";
+    let newUserMode = routeMode === "Edit Route" ? "edit" : "view";
+    setRouteMode(newRouteMode);
+    props.changeUserMode(newUserMode);
+  }
+
+  //function parse csv file located at csvFilePath to be parsed into an array
+  function loadCSV(){
+    csv(csvFilePath).then(data => {
+      let csvWaypoints;
+      for(let i = 0; i < data.length; i++){
+        const newCoordinate = {long: data[i].lng, lat: data[i].lat};
+        props.addMarker(newCoordinate);
+        props.waypoints.map((newCoordinate, i) => (
+          <div style={{overflow: "auto", fontSize: "14px"}} >
+        <span>
+          <strong>Point#:</strong> {i},
+          <strong>   Latitude:</strong> {newCoordinate.lat},
+          <strong>   Longitude:</strong> {newCoordinate.lng}
+        </span><br/>
+      </div>
+        ));
+      }
+    })
+  }
 
   return (
-    <div>
-       <h4 style={{paddingLeft: "12px"}}>Connections</h4>
-       <p style={{paddingLeft: "12px"}}>Bit rate:  {bit_rate}</p>
-       <p style={{paddingLeft: "12px"}}>Link quality:  {link_quality}</p>
-       <p style={{paddingLeft: "12px"}}>Signal:  {signal}</p>
-       <p style={{paddingLeft: "12px"}}>Noise:   {noise}</p>
+  <div>
 
-     </div>
+    <button onClick={changeRouteMode}>{routeMode}</button><br/>
+  
+    <button 
     
+      style = {{
+        position: 'relative',
+        right : -90,
+        top: -22
+      }}
+      onClick = {loadCSV} > Load CSV</button><br/>
+ 
+    <form style={{visibility: visibility}}  onSubmit={addCoordinate}>
+      <label for="latitude">latitude:</label>
+      <input type="number" step="any" id="latitude" name="latitude"></input>
+      <br></br>
+      <label for="longitude">Longitude:</label>
+      <input type="number" step="any" id="longitude" name="longitude" ></input>
+      <br></br>
+      <input type="submit" value="Add Coordinate"></input>
+    </form>
+
+    {props.waypoints.map((coordinate, idx) => (
+      <div style={{overflow: "auto", fontSize: "14px"}} >
+        <span>
+          <strong>Point#:</strong> {idx+1},
+          <strong>   Latitude:</strong> {coordinate.lat},
+          <strong>   Longitude:</strong> {coordinate.lng}
+        </span><br/>
+      </div>
+    ))}
+  </div> 
   );
-}
-
-
+  }
