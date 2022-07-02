6c98b1c798cf117f5fe508b9bda55fade0e800ce
shivamdesai04 <shivam.desai@ucalgary.ca>
2022-07-02 16:24:21 -0700

    src/components/VehicleHealth.js

---
+++ build/robot_gui_bridge/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/shivamdesai/Desktop/rover/react-rover-ui/devel/.private/robot_gui_bridge:$CMAKE_PREFIX_PATH"
+export PWD='/home/shivamdesai/Desktop/rover/react-rover-ui/build/robot_gui_bridge'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/shivamdesai/Desktop/rover/react-rover-ui/devel/.private/robot_gui_bridge/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/shivamdesai/Desktop/rover/react-rover-ui/src/robot_gui_bridge:$ROS_PACKAGE_PATH"
