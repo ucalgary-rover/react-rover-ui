82832f8638b681607a40fe8ad25eb36520efb2a9
CarterM2607 <cartermar211@gmail.com>
2022-07-24 13:25:34 -0600

    added new launch files and bash scripts

---
+++ src/robot_gui_bridge/scripts/ui-startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#edit the following lines of the bash scripts to match the location of the directories
+cd ../react-rover-ui/
+npm start
---
+++ src/robot_gui_bridge/scripts/xp-map-server-startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ../express-map-server/
+npm start
